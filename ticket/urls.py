300b7c727f336eb6ed3dba6c041ca199f61fd219
numanturle <neptun@solarsystem.local>
2022-05-01 19:44:00 +0300

    readme.md

--- ticket/urls.py
+++ ticket/urls.py
@@ -23,10 +23,6 @@ urlpatterns = [
 
     path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
 
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-
     path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
          name='password_reset_complete'),
