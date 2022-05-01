656f8c3eb35140650b1b3d663cfef612a6631acb
numanturle <neptun@solarsystem.local>
2022-05-01 19:29:39 +0300

    change settings

--- ticket/settings.py
+++ ticket/settings.py
@@ -134,22 +134,6 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 
 
-# social auth configs for github
-SOCIAL_AUTH_GITHUB_KEY = str(os.getenv('GITHUB_KEY'))
-SOCIAL_AUTH_GITHUB_SECRET = str(os.getenv('GITHUB_SECRET'))
-
-# social auth configs for google
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = str(os.getenv('GOOGLE_KEY'))
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = str(os.getenv('GOOGLE_SECRET'))
-
-# email configs
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
-
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 
 
