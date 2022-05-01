ed444db15b500f49c0301ce9e2d473a1cc454eb6
numanturle <neptun@solarsystem.local>
2022-05-01 19:36:27 +0300

    hardcoded security

--- ticket/settings.py
+++ ticket/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't=q92@7r@8-)h5*cc^c=af9+y2tnlnv7a-$!0va4!y=5=eaq4l'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
